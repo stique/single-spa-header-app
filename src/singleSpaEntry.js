94ed9ba2b0f371600e5eb73098b84e40e7fae1b3
Kirill Rebrikov <stique@reg.ru>
2020-02-18 18:50:09 +0400

    build

--- src/singleSpaEntry.js
+++ src/singleSpaEntry.js
@@ -1,5 +1,4 @@
 import Vue from 'vue';
-import VueToastr from 'vue-toastr';
 import singleSpaVue from 'single-spa-vue';
 import { BootstrapVue } from 'bootstrap-vue';
 import App from './App.vue';
@@ -9,12 +8,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.use(BootstrapVue);
-
-Vue.use(VueToastr, {
-  defaultPosition: 'toast-top-right',
-  defaultPreventDuplicates: true,
-  defaultTimeout: 0,
-});
 
 Vue.config.productionTip = false;
 
