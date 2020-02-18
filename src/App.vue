c1eef494363ddbec1b5340583499bef4d602ce7a
Kirill Rebrikov <stique@reg.ru>
2020-02-18 18:41:15 +0400

    process

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,8 @@
   <div id="vue-app">
       <div id="vue-component" class="container">
           <div class="main">
-            <h1><img src="https://vuejs.org/images/logo.png" width="50" height="50"> Vue App</h1>
-            <hr>
             <p>
-                This is the Vue section page.
-                This section is displayed when url starts with "/vue" prefix.
+                Header
             </p>
             <p id="links">
                 <router-link to="/">List</router-link>
